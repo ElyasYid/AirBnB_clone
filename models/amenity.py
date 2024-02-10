09ff26401bf253f9563ab8bcf59b0682ed25dc29
ElyasYid <elyassayle27@gmail.com>
2024-02-10 02:40:34 -0800

    classes

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Rep an amenity.
+    Attributes:
+        name (str): name of the amenity.
+    """
+
+    name = ""
